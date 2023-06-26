4f8e3c6eb0845cdbbd49c3e852db375c1386c493
Raunchers <maethridge@gmail.com>
2023-06-25 20:12:28 -0400

    Created go templates for the following pages: Home, About, Contact, Generals Quarters, Majors Suite and the booking page. Added javascript for the generals/majors book now button. Fixed routes to allow web server to pull up proper pages.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,6 +18,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/generals-quaters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
 
 	// create a file server, a place to get static files from
 	fileServer := http.FileServer(http.Dir("./static/"))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -27,7 +27,7 @@ func NewHandlers(r *Repository) {
 	Repo = r
 }
 
-// Home is the home page handler
+// Home renders the home page
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	// Grab the remote IP address, returns as a string
 	remoteIP := r.RemoteAddr
@@ -37,7 +37,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
-// About is the about page handler
+// About renders the about page
 func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// Perform some logic
 	stringMap := make(map[string]string)
@@ -49,4 +49,29 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+
+// Reservation renders the Reservation page
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Availability renders the Availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "search-availability.page.tmpl", &models.TemplateData{})
+}
+
+// Generals renders the Generals room page
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors renders the Majors room page
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// Contact renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, "contact.page.tmpl", &models.TemplateData{})
 }
